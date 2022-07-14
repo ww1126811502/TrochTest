95322e4534d817f2157d8e48be817c536ea15180
牧川ww <1126811502@qq.com>
2022-07-15 00:11:52 +0800

    测试循环

--- nn_CIFAR10.py
+++ nn_CIFAR10.py
@@ -9,12 +9,44 @@ from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Sequential
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-dataset = torchvision.datasets.CIFAR10("datasets", train=False, transform=torchvision.transforms.ToTensor(),
-                                       download=True)
-"""创建数据集，存放在datasets，为测试数据集，且转换为tensor"""
 
-dataloader = DataLoader(dataset, 64)
-"""用dataloader取数据集，设置每次打包64个"""
+def main():
+    print("加载成功")
+
+
+def moduletest():
+    dataset = torchvision.datasets.CIFAR10("datasets", train=False, transform=torchvision.transforms.ToTensor(),
+                                           download=True)
+    """创建数据集，存放在datasets，为测试数据集，且转换为tensor"""
+
+    dataloader = DataLoader(dataset, 64)
+    """用dataloader取数据集，设置每次打包64个"""
+
+    module = MyModule()
+    # 测试部分：
+    # input = torch.ones((64, 3, 32, 32))
+    # """torch.ones，模拟一个与输入格式相同的tensor，便于进行测试和检查"""
+    # output = module(input)
+    # print(output.shape)
+    #
+    # writer = SummaryWriter("logs")
+    # writer.add_graph(module, input)
+    # """利用graph展示，可以清晰看到数据的每一步转化及其参数"""
+    #
+    #
+    # writer.close()
+
+    loss = nn.CrossEntropyLoss()
+    step = 0
+    for data in dataloader:
+        imgs, targets = data
+        """从dataloader中取到的imgs和targets均为数组"""
+        outputs = module(imgs)
+        # print(outputs)
+        result_loss = loss(outputs, targets)
+        print(result_loss)
+        result_loss.backward()
+        print(result_loss)
 
 
 class MyModule(nn.Module):
@@ -23,11 +55,11 @@ class MyModule(nn.Module):
     def __init__(self):
         super(MyModule, self).__init__()
         self.model1 = Sequential(
-            Conv2d(3, 32, 5, padding=2),
+            Conv2d(3, 32, 5, 1, padding=2),
             MaxPool2d(2),
-            Conv2d(32, 32, 5, padding=2),
+            Conv2d(32, 32, 5, 1, padding=2),
             MaxPool2d(2),
-            Conv2d(32, 64, 5, padding=2),
+            Conv2d(32, 64, 5, 1, padding=2),
             MaxPool2d(2),
             Flatten(),
             Linear(1024, 64),
@@ -35,31 +67,12 @@ class MyModule(nn.Module):
         )
 
     def forward(self, x):
-        return self.model1(x)
+        x = self.model1(x)
+        return x
 
 
-module = MyModule()
-#测试部分：
-# input = torch.ones((64, 3, 32, 32))
-# """torch.ones，模拟一个与输入格式相同的tensor，便于进行测试和检查"""
-# output = module(input)
-# print(output.shape)
-#
-# writer = SummaryWriter("logs")
-# writer.add_graph(module, input)
-# """利用graph展示，可以清晰看到数据的每一步转化及其参数"""
-#
-#
-# writer.close()
-
-loss = nn.CrossEntropyLoss()
-step = 0
-for data in dataloader:
-    imgs, targets = data
-    """从dataloader中取到的imgs和targets均为数组"""
-    outputs = module(imgs)
-    #print(outputs)
-    result_loss = loss(outputs, targets)
-    print(result_loss)
-    result_loss.backward()
-    print(result_loss)
+if __name__ == '__main__':
+    module = MyModule()
+    input = torch.ones((64, 3, 32, 32))
+    output = module(input)
+    print(output.shape)
--- test_totally_train.py
+++ test_totally_train.py
@@ -2,49 +2,95 @@
 # @Time : 2022/7/13 8:03
 # @Author : 牧川
 # @File : test_totally_train.py
+import datetime
 
 import torchvision
 import torch.nn
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
 from nn_CIFAR10 import MyModule
 
-#准备数据集
+#初始化一个device
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+print(f"当前的device类型：{device}")
+
+# 准备数据集
 train_data = torchvision.datasets.CIFAR10("datasets", train=True, transform=torchvision.transforms.ToTensor(),
                                           download=True)
 test_data = torchvision.datasets.CIFAR10("datasets", train=False, transform=torchvision.transforms.ToTensor(),
                                          download=True)
-#数据集长度
+# 数据集长度
 train_data_size = len(train_data)
 test_data_size = len(test_data)
 print(f"训练数据集的长度：{test_data_size}\n测试数据集的长度：{test_data_size}")
 
-#准备dataloader：
+# 准备dataloader：
 train_dataloader = DataLoader(train_data, 64)
 test_dataloader = DataLoader(test_data, 64)
 
-#搭建神经网络：
+# 搭建神经网络：
 mymodle = MyModule()
-#损失函数：
+mymodle.to(device)
+# 损失函数：
 loss_fn = torch.nn.CrossEntropyLoss()
-#优化器：
+loss_fn.to(device)
+# 优化器：
 learning_rate = 0.01
 optim = torch.optim.SGD(mymodle.parameters(), lr=learning_rate)
 
-#声明一些计数变量
+# 声明一些计数变量
 total_train_step = 0
 total_test_step = 0
 epoch = 10
+# 添加tensorboard
+writer = SummaryWriter("logs")
 
 for i in range(epoch):
-    print(f"第{i+1}轮训练开始：")
-    #训练步骤:
-    for data in train_data:
+    print(f"第{i + 1}轮训练开始：")
+    # 训练步骤:
+    for data in train_dataloader:
         imgs, targets = data
+        imgs = imgs.to(device)
+        targets = targets.to(device)
         outputs = mymodle(imgs)
         # print(outputs)
-        result_loss = mymodle(outputs, targets)
-        optim.zero_grad()
-        result_loss.backward()
-        optim.step()
+        result_loss = loss_fn(outputs, targets)  # 损失函数的计算
+        optim.zero_grad()  # 梯度归零
+        result_loss.backward()  # 梯度反向传播
+        optim.step()  # 优化器
         total_train_step += 1
-    print(f"第{i + 1}轮训练结束，")
+
+        if total_train_step % 100 == 0:
+            print(f"训练次数{total_train_step},当前loss:{result_loss.item()}")
+            writer.add_scalar("train_loss", result_loss.item(), total_train_step)
+
+    total_test_loss = 0
+    total_accuracy = 0
+    with torch.no_grad():
+        # 每次训练结束后用测试集验证一次结果，no_grad即不对梯度造成影响
+        for data in test_dataloader:
+            imgs, targets = data
+            imgs = imgs.to(device)
+            targets = targets.to(device)
+            outputs = mymodle(imgs)
+            loss = loss_fn(outputs, targets)
+            total_test_loss += loss
+
+            #正确率计算：
+            accuracy = (outputs.argmax(1) == targets).sum()
+            """outputs.argmax(1)将每一行（原结构是对每一类的预测概率），改为每一行最大值的index，实际代表预测的类别"""
+            """== targets即将预测结果与目标结果对比，得到true、false组成的数组"""
+            """.sum()即将结果求和，实际代表命中的次数"""
+            total_accuracy += accuracy
+            """将本loader中数据的命中次数加至总和"""
+    print(f"第{i + 1}轮训练结束，整体测试集上的loss:{total_test_loss}")
+    print(f"第{i + 1}轮训练结束，整体测试集上的正确率:{total_accuracy/test_data_size}")
+    writer.add_scalar("test_loss", loss.item(), total_test_step)
+    writer.add_scalar("test_accuracy", total_accuracy/test_data_size, total_test_step)
+    total_test_step += 1
+
+writer.close()
+
+torch.save(mymodle.state_dict(), f"mymodle.pth")
+print("训练已完成，模型已保存！")
